040cecc372e7dcc03827f38d9ec4c4f6593d4500
Dave Shoup <dave.shoup@gmail.com>
2026-03-12 22:17:11 -0400

    add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    include: ["src/**/*.test.ts"],
+    globals: false,
+    restoreMocks: true,
+    testTimeout: 10_000,
+  },
+});

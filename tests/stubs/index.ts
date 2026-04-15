4e95c249273fdcc079ee76e4d842129c38764bb8
Bernie Chen <bchen@confluent.io>
2026-04-15 14:05:36 -0500

    feat(oauth): add fetch and crypto wrappers to node-deps for test stubbing

--- tests/stubs/index.ts
+++ tests/stubs/index.ts
@@ -1,1 +1,9 @@
 export { createStubAdmin, type StubbedAdmin } from "./admin.js";
+export {
+  createFsWrappers,
+  type StubbedFsWrappers,
+  createFetchWrapper,
+  type StubbedFetchWrapper,
+  createCryptoWrapper,
+  type StubbedCryptoWrapper,
+} from "./node-deps.js";

cabd2e4c79e1c9e49fda23e284ace09ee5be8eb3
Bernie Chen <bchen@confluent.io>
2026-04-23 14:10:34 -0500

    fix: remove stubs

--- tests/stubs/index.ts
+++ tests/stubs/index.ts
@@ -1,9 +1,5 @@
 export { createStubAdmin, type StubbedAdmin } from "./admin.js";
 export {
-  createStubAuthContext,
-  type StubbedAuthContext,
-} from "./auth-context.js";
-export {
   createCryptoWrapper,
   createFetchWrapper,
   createFsWrappers,

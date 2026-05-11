e5400a13371783e162178dbbdc879b032a8bb8c6
Noel Cothren <ncothren@confluent.io>
2026-05-11 12:06:32 -0600

    login pages: replace plain string w/ styled HTML

---
+++ src/confluent/oauth/callback-pages.ts
@@ -0,0 +1,58 @@
+import { fs } from "@src/confluent/node-deps.js";
+
+const SUCCESS_TEMPLATE_FILE = "callback-success.html";
+const FAILURE_TEMPLATE_FILE = "callback-failure.html";
+
+const PARTIALS: Record<string, string> = {
+  "{#include styles/}": "styles.html",
+  "{#include logo/}": "logo.html",
+};
+
+const TEMPLATES_DIR_URL = new URL(
+  "../../../assets/oauth-templates/",
+  import.meta.url,
+);
+
+let cachedSuccessPage: string | undefined;
+let cachedFailureTemplate: string | undefined;
+
+function readTemplate(fileName: string): string {
+  return fs.readFileSync(new URL(fileName, TEMPLATES_DIR_URL), "utf-8");
+}
+
+function inlinePartials(template: string): string {
+  return Object.entries(PARTIALS).reduce(
+    (acc, [directive, fileName]) =>
+      acc.replace(directive, readTemplate(fileName)),
+    template,
+  );
+}
+
+function escapeHtml(value: string): string {
+  return value
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
+export function renderSuccessPage(): string {
+  if (cachedSuccessPage === undefined) {
+    cachedSuccessPage = inlinePartials(readTemplate(SUCCESS_TEMPLATE_FILE));
+  }
+  return cachedSuccessPage;
+}
+
+export function renderErrorPage(message: string): string {
+  if (cachedFailureTemplate === undefined) {
+    cachedFailureTemplate = inlinePartials(readTemplate(FAILURE_TEMPLATE_FILE));
+  }
+  return cachedFailureTemplate.replace("{{error}}", escapeHtml(message));
+}
+
+/** Test-only: reset the module-level template cache. */
+export function _resetCallbackPageCacheForTests(): void {
+  cachedSuccessPage = undefined;
+  cachedFailureTemplate = undefined;
+}
